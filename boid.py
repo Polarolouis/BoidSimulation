ddb958df3bb3cfccf9b88359fbb64351b7716d13
Louis Lacoste <lordof20th@hotmail.fr>
2022-04-20 12:29:33 +0200

    Unfinished work on the vectorization of the simulation

--- boid.py
+++ boid.py
@@ -1,121 +1,153 @@
 import math
 import random
+import numpy as np
 
 class Boid:
     """Boid class"""
     near_distance = 50 # Distance to be considered near
     chaotic_probability = 0
     weight_of_cohesion = 1
-    def __init__(self, x_pos, y_pos, speed, angle, width, height, bouncing):
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.speed = speed
-        self.angle = angle
+    def __init__(self, x_pos, y_pos, x_vel, y_vel, width, height, bouncing):
+        # Initialise the boid position and velocity
+        self.position = np.array([[x_pos], [y_pos]], dtype=np.float64)
+        self.velocity = np.array([[x_vel], [y_vel]], dtype=np.float64)
+        self.acceleration = np.array([[0], [0]], dtype=np.float64)
+
         self.width = width
         self.height = height
-        self.x_vel = self.speed * math.cos(self.angle)
-        self.y_vel = self.speed * math.sin(self.angle)
+
         self.radius = 5
         self.bouncing = bouncing
 
-        self.near_boids = []
-    
-    def update_velocity(self):
-        """Update the velocity of the boid"""
-        # Alignment behaviour
-        alignment_angle = self.alignment()
-        self.angle = alignment_angle
-        
-        # Cohesion behaviour
-        # cohesion_angle = self.cohesion()
-        # self.angle += cohesion_angle
-        
-        # Collision avoidance behaviour
-        self.avoid_collision()
-        
-        # Chaotic angle
-        if random.random()< self.chaotic_probability:
-            self.angle = 0.5 * random.random() * 360 + 0.5 * self.angle
+        self.near_boids = []   
 
-        # Update the velocity
-
-        self.x_vel = self.speed * math.cos(self.angle)
-        self.y_vel = self.speed * math.sin(self.angle)
-
-    def alignment(self):
-        """Alignment behaviour to steer towards the average heading of the boids in the near_boids list"""
-        # Calculate the average angle of the boids
-        angle_avg = 0
-        for boid in self.near_boids:
-            angle_avg += boid.angle
-        if self.near_boids:
-            angle_avg /= len(self.near_boids)
-        return angle_avg
-
-    def cohesion(self):
-        """Cohesion behaviour to steer towards the average position of the boids in the near_boids list"""
-        # Calculate the average position of the boids
-        x_avg = 0
-        y_avg = 0
-        for boid in self.near_boids:
-            x_avg += boid.x_pos
-            y_avg += boid.y_pos
-        if self.near_boids:
-            x_avg /= len(self.near_boids)
-            y_avg /= len(self.near_boids)
-        return math.atan2(y_avg - self.y_pos, x_avg - self.x_pos)
-
-    def avoid_collision(self):
-        """Avoid collision behaviour"""
-        pass
-    
-
-    def update_position(self):
-        """Update the position of the boid"""
-        # statement to check if the boid is out of bounds
-        # and bounce it back if it is
-        if self.bouncing:
-            if self.x_pos + self.x_vel > self.width:
-                self.x_vel = - self.x_vel
-            elif self.x_pos + self.x_vel < 0:
-                self.x_vel = - self.x_vel
-            
-            if self.y_pos + self.y_vel > self.height:
-                self.y_vel = - self.y_vel
-            elif self.y_pos + self.y_vel < 0:
-                self.y_vel = - self.y_vel
-        else:
-            if self.x_pos + self.x_vel > self.width:
-                self.x_pos = 0
-                self.x_vel = self.x_vel
-            elif self.x_pos + self.x_vel < 0:
-                self.x_pos = self.width
-                self.x_vel = self.x_vel
-            
-            if self.y_pos + self.y_vel > self.height:
-                self.y_pos = 0
-                self.y_vel = self.y_vel
-            elif self.y_pos + self.y_vel < 0:
-                self.y_pos = self.height
-                self.y_vel = self.y_vel            
-        
-        self.x_pos += self.x_vel
-        self.y_pos += self.y_vel
-
-    def bbox(self):
-        """Returns the bounding box of the boid"""
-        return (self.x_pos - self.radius, self.y_pos - self.radius, self.x_pos + self.radius, self.y_pos + self.radius)
-    
+# Flock calculation
     def find_near_boids(self, boids):
         """Sets a list of boids that are within a certain distance"""
         self.near_boids = []
         for boid in boids:
-            if self.distance(boid) < self.near_distance:
+            if (not np.array_equal(self.position, boid.position)) and self.distance(boid) < self.near_distance:
                 self.near_boids.append(boid)
-    
+
     def distance(self, other_boid):
         """Return the distance between two boids"""
-        return math.sqrt((self.x_pos - other_boid.x_pos)**2 + (self.y_pos - other_boid.y_pos)**2)
+        return np.linalg.norm(self.position - other_boid.position)
+
+# Flock behaviour
+    def alignment(self):
+        """Alignment behaviour to steer towards the average heading of the boids in the near_boids list
+        Returns : the correction to add to the velocity""" 
+        # Calculate the average heading of the boids
+        heading_avg = np.array([[0], [0]], dtype=np.float64)
+        for boid in self.near_boids:
+            heading_avg += boid.velocity
+        if self.near_boids:
+            heading_avg /= len(self.near_boids)
+        heading_correction = heading_avg - self.velocity
+        return heading_correction
+
+    def cohesion(self):
+        """Cohesion behaviour to steer towards the average position of the boids in the near_boids list
+        Returns : the correction to add to the velocity""" 
+        # Calculate the average position of the boids
+        position_avg = np.array([[0], [0]], dtype=np.float64)
+        for boid in self.near_boids:
+            position_avg += boid.position
+        if self.near_boids:
+            position_avg /= len(self.near_boids)
+        correction_to_avg = position_avg - self.position
+        return correction_to_avg
+
+    def separation(self):
+        """Separation behaviour to avoid collisions with other boids
+        Returns : the correction to add to the velocity"""
+        separation_correction = np.array([[0], [0]], dtype=np.float64)
+        for boid in self.near_boids:
+            distance_to_boid = self.distance(boid)
+            diff = np.asarray(self.position - boid.position, dtype=np.float64)
+            np.divide(diff, np.asarray(distance_to_boid, dtype=np.float64), out=diff)
+            separation_correction += diff
+        if self.near_boids:
+            separation_correction /= len(self.near_boids)
+        return separation_correction
+
+    def apply_force(self, force):
+        """Apply a force to the boid by incrementing the acceleration"""
+        self.acceleration += force
+
+    def apply_rules(self):
+        """Apply the rules of the flock to the boid"""
+        self.apply_force(self.separation())
+        self.apply_force(self.alignment())
+        self.apply_force(self.cohesion())
+
+    def check_edges(self):
+        """Check if the boid is out of bounds"""
+        # If bouncing is on, bounce the boid back into the screen
+        if self.bouncing:
+            velocity = self.velocity
+            if self.position[0] < self.radius:
+                velocity[0] = -self.velocity[0]
+            if self.position[0] > self.width - self.radius:
+                velocity[0] = -self.velocity[0]
+            if self.position[1] < self.radius:
+                velocity[1] = -self.velocity[1]
+            if self.position[1] > self.height - self.radius:
+                velocity[1] = -self.velocity[1]
+            return velocity
+        else:
+            # If bouncing is off, check if the boid is out of bounds
+            # And if it is, set the boid to the opposite side
+            position = self.position
+            if self.position[0] < self.radius:
+                position[0] = self.width - self.radius
+            if self.position[0] > self.width - self.radius:
+                position[0] = self.radius
+            if self.position[1] < self.radius:
+                position[1] = self.height - self.radius
+            if self.position[1] > self.height - self.radius:
+                position[1] = self.radius
+            return position
+
+# Update the boid
+    def update(self):
+        """Update the velocity and the position of the boid"""
+        # We need to have found the near_boids list before we can apply the rules
+        self.apply_rules()
+        # The acceleration is the sum of the forces
+
+        # Update the velocity
+        self.velocity += self.acceleration
+        
+        # Check if the boid is out of bounds and apply the correction
+        if self.bouncing:
+            # If we bounce the output is the velocity
+            self.velocity = self.check_edges()
+        else:
+            # If we don't bounce the output is the position
+            self.position = self.check_edges()
+
+        # Update the position
+        self.position += self.velocity
+
+        # Reset the acceleration
+        self.acceleration = np.array([[0], [0]], dtype=np.float64)
+
+# Methods for the display
+    def get_coords(self):
+        """Returns the coordinates of the boid"""
+        x_pos, y_pos = self.position
+        return (*x_pos, *y_pos) # unpacking the tuple
+
+    def get_velocity(self):
+        """Returns the velocity of the boid"""
+        x_vel, y_vel = self.velocity
+        return (*x_vel, *y_vel)
+
+    def bbox(self):
+        """Returns the bounding box of the boid"""
+        x_pos, y_pos = self.position
+        return (x_pos - self.radius, y_pos - self.radius, x_pos + self.radius, y_pos + self.radius)
 
 class SimulationSpace:
     """Simulation space class"""
@@ -124,20 +156,21 @@ class SimulationSpace:
         self.width = width
         self.height = height
         self.boids = []
+        self.iteration = 0
     
     def populate(self, number_of_boids, bouncing=True):
         """Populate the simulation space with boids"""
         for _ in range(number_of_boids):
             x_pos = random.randint(0, self.width)
             y_pos = random.randint(0, self.height)
-            speed = 5 # random.randint(1, 10)
-            angle = random.randint(0, 360)
-            boid = Boid(x_pos, y_pos, speed, angle, self.width, self.height, bouncing=bouncing)
+            x_vel = 0.01 # random.randint(1, 5)
+            y_vel = 0.01 # random.randint(1, 5)
+            boid = Boid(x_pos, y_pos, x_vel, y_vel, self.width, self.height, bouncing=bouncing)
             self.boids.append(boid)
     
     def next_step(self):
         """Update the simulation space"""
+        self.iteration += 1
         for boid in self.boids:
             boid.find_near_boids(self.boids)
-            boid.update_velocity()
-            boid.update_position()
+            boid.update()
--- display.py
+++ display.py
@@ -6,25 +6,102 @@ window.title("Boid Simulation")
 
 show_range = False
 
-size = 500
-space = bd.SimulationSpace(size, size)
-space.populate(20, bouncing=False)
+SIZE = 500
 
-window.configure(width=size, height=size)
-space.canvas = tkinter.Canvas(window, width=size, height=size)
-space.canvas.pack()
+space = bd.SimulationSpace(SIZE, SIZE)
 
-for boid in space.boids:
-    if show_range:
-        boid.near_canvas_id = space.canvas.create_oval(boid.x_pos - boid.near_distance, boid.y_pos - boid.near_distance, boid.x_pos + boid.near_distance, boid.y_pos + boid.near_distance, fill="", outline="gray")
-    boid.canvas_id = space.canvas.create_line(boid.x_pos, boid.y_pos, boid.x_pos+boid.x_vel, boid.y_pos+boid.y_vel, fill="red", width=5, arrow=tkinter.LAST)
-def move_boid_canvas(space):
-    """Move the boid canvas and update the position"""
+canvas = tkinter.Canvas(window, width=SIZE, height=SIZE)
+canvas.pack()
+paused = True
+
+
+def initialise_simulation():
+    """Initialise the simulation"""
+    global space
+    global paused
+    del space
+    space = bd.SimulationSpace(SIZE, SIZE)
+    space.populate(20, bouncing=False)
+    paused = True
+    canvas.delete('all')
     for boid in space.boids:
+        x_pos, y_pos = boid.get_coords()
+        x_vel, y_vel = boid.get_velocity()
         if show_range:
-            space.canvas.coords(boid.near_canvas_id, boid.x_pos - boid.near_distance, boid.y_pos - boid.near_distance, boid.x_pos + boid.near_distance, boid.y_pos + boid.near_distance)
-        space.canvas.coords(boid.canvas_id, boid.x_pos, boid.y_pos, boid.x_pos+boid.x_vel, boid.y_pos+boid.y_vel)
-    
-    window.after(10, lambda: (space.next_step(), move_boid_canvas(space)))
-space.canvas.after(10, lambda: (space.next_step(),move_boid_canvas(space)) )
+            boid.near_canvas_id = canvas.create_oval(x_pos - boid.near_distance, y_pos - boid.near_distance, x_pos + boid.near_distance, y_pos + boid.near_distance, fill="", outline="gray")
+        boid.canvas_id = canvas.create_line(x_pos, y_pos, x_pos+x_vel, y_pos+y_vel, fill="red", width=5, arrow=tkinter.LAST)
+window.configure(width=SIZE, height=SIZE)
+
+# Top frame
+top_frame = tkinter.Frame(window)
+top_frame.pack(side=tkinter.TOP)
+
+## Iteration counter label
+iteration_label = tkinter.Label(top_frame, text="Iteration: 0")
+iteration_label.pack(side=tkinter.LEFT)
+
+def update_iteration_label(space):
+    """Update the iteration label"""
+    iteration_label.configure(text=f"Iteration: {space.iteration}")
+
+def toggle_paused():
+    """Toggle the paused state"""
+    global paused
+    paused = not paused
+
+# Control Frame Bottom
+command_frame = tkinter.LabelFrame(window, text="Commands")
+command_frame.pack(side=tkinter.BOTTOM, expand=True, fill=tkinter.BOTH)
+
+## Control Frame Left
+control_frame = tkinter.LabelFrame(command_frame, text="Controls of the simulation")
+control_frame.pack(side=tkinter.LEFT)
+
+### Start Button
+start_button = tkinter.Button(control_frame, text="Start", command=lambda: start_simulation(space))
+start_button.pack()
+
+### Pause/Resume Button
+resume_pause_button = tkinter.Button(control_frame, text="Resume/Pause", \
+    command=lambda: (toggle_paused() ,move_boid_canvas(space)))
+resume_pause_button.pack()
+
+### Next Step Button
+next_step_button = tkinter.Button(control_frame, text="Next Step", \
+    command=lambda: (space.next_step(), move_boid_canvas(space)))
+next_step_button.pack()
+
+## Window Frame Right
+settings_frame = tkinter.LabelFrame(command_frame, text="Settings")
+settings_frame.pack(side=tkinter.RIGHT)
+
+### Reinitialise Button
+reinitialise_button = tkinter.Button(settings_frame, text="Reinitialise", \
+    command=lambda: (initialise_simulation(), move_boid_canvas(space)))
+reinitialise_button.pack()
+
+def move_boid_canvas(space):
+    """Move the boid canvas"""
+    update_iteration_label(space)
+    for boid in space.boids:
+        x_pos, y_pos = boid.get_coords()
+        x_vel, y_vel = boid.get_velocity()
+        if show_range:
+            canvas.coords(boid.near_canvas_id, x_pos - boid.near_distance, y_pos - boid.near_distance, x_pos + boid.near_distance, y_pos + boid.near_distance)
+        canvas.coords(boid.canvas_id, x_pos, y_pos, x_pos+x_vel, y_pos+y_vel)
+
+def iterate_simulation(space):
+    """Move the boid canvas and update the position"""
+    if not paused:
+        move_boid_canvas(space)
+        space.next_step()
+    window.after(10, lambda: iterate_simulation(space))
+
+def start_simulation(space):
+    """Start the simulation"""
+    toggle_paused()
+    iterate_simulation(space)
+
+
+initialise_simulation()
 window.mainloop()
