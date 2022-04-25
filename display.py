89a45612b6c53d0bf11c94838a71d1dda7a411c0
Louis Lacoste <lordof20th@hotmail.fr>
2022-04-25 15:31:40 +0200

    Added the interface support of the goal force

--- display.py
+++ display.py
@@ -15,6 +15,9 @@ COHESION = True
 WIND = False
 WIND_SPEED = 0
 WIND_DIRECTION = 0
+GOAL = False
+GOAL_X = 0
+GOAL_Y = 0
 
 sim_space = None
 
@@ -59,87 +62,10 @@ button_reset.pack(side=tkinter.RIGHT)
 parameters_frame = tkinter.Frame(root)
 parameters_frame.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 
-## Bouncing checkbox 
-bouncing_boolean = tkinter.BooleanVar()
-bouncing_checkbox = tkinter.Checkbutton(parameters_frame, text="Bouncing", font=("Helvetica", 16), variable=bouncing_boolean, onvalue=True, offvalue=False)
-if BOUNCING:
-    bouncing_checkbox.select()
-bouncing_checkbox.pack()
-
-## Number of boids slider
-number_of_boids_slider = tkinter.Scale(parameters_frame, from_=1, to=1000, orient=tkinter.HORIZONTAL, length=200, label="Number of boids", font=("Helvetica", 16))
-number_of_boids_slider.set(NUMBER_OF_BOIDS)
-number_of_boids_slider.pack()
-
-## Number of steps slider
-label_number_of_steps = tkinter.Label(parameters_frame, text="Number of steps", font=("Helvetica", 16))
-label_number_of_steps.pack()
-var_number_of_steps_spinbox = tkinter.StringVar()
-var_number_of_steps_spinbox.set(NUMBER_OF_STEPS)
-number_of_steps_spinbox = tkinter.Spinbox(parameters_frame, from_=0, to=1_000_000, textvariable=var_number_of_steps_spinbox, font=("Helvetica", 16))
-number_of_steps_spinbox.pack()
-
-## Forces frame
-forces_frame = tkinter.LabelFrame(parameters_frame, text="Forces", font=("Helvetica", 16))
-forces_frame.pack()
-
-### Alignment checkbox
-alignment_boolean = tkinter.BooleanVar()
-alignment_checkbox = tkinter.Checkbutton(forces_frame, text="Alignment", font=("Helvetica", 16), variable=alignment_boolean, onvalue=True, offvalue=False)
-if ALIGNMENT:
-    alignment_checkbox.select() 
-alignment_checkbox.pack()
-
-### Cohesion checkbox
-cohesion_boolean = tkinter.BooleanVar()
-cohesion_checkbox = tkinter.Checkbutton(forces_frame, text="Cohesion", font=("Helvetica", 16), variable=cohesion_boolean, onvalue=True, offvalue=False)
-if COHESION:
-    cohesion_checkbox.select()
-cohesion_checkbox.pack()
-
-### Separation checkbox
-separation_boolean = tkinter.BooleanVar()
-separation_checkbox = tkinter.Checkbutton(forces_frame, text="Separation", font=("Helvetica", 16), variable=separation_boolean, onvalue=True, offvalue=False)
-if SEPARATION:
-    separation_checkbox.select()
-separation_checkbox.pack()
-
-### Wind checkbox
-
-def toggle_wind_parameters():
-    """ Show the wind parameters if the wind checkbox is selected """
-    print("toggle_wind_parameters")
-    if wind_boolean.get():
-        wind_parameters_frame.pack()
-    else:
-        wind_parameters_frame.pack_forget()
-
-wind_boolean = tkinter.BooleanVar()
-wind_checkbox = tkinter.Checkbutton(forces_frame, text="Wind", font=("Helvetica", 16), variable=wind_boolean, onvalue=True, offvalue=False, command=toggle_wind_parameters)
-if WIND:
-    wind_checkbox.select()
-wind_checkbox.pack()
-
-#### Wind parameters
-wind_parameters_frame = tkinter.LabelFrame(forces_frame, text="Wind Settings", font=("Helvetica", 16))
-wind_parameters_frame.pack()
-
-##### Wind speed slider
-wind_speed_slider = tkinter.Scale(wind_parameters_frame, from_=0, to=10, orient=tkinter.HORIZONTAL, 
-    length=200, tickinterval=2, label="Wind speed", font=("Helvetica", 16))
-wind_speed_slider.set(0)
-wind_speed_slider.pack()
-
-##### Wind direction slider
-wind_direction_slider = tkinter.Scale(wind_parameters_frame, from_=0, to=360, orient=tkinter.HORIZONTAL, 
-    length=200, label="Wind direction", font=("Helvetica", 16))
-wind_direction_slider.set(0)
-wind_direction_slider.pack()
-
 ## Validation button
 def validate_parameters():
     """Set the parameters"""
-    global NUMBER_OF_BOIDS, NUMBER_OF_STEPS, BOUNCING, ALIGNMENT, SEPARATION, COHESION, WIND, WIND_SPEED, WIND_DIRECTION
+    global NUMBER_OF_BOIDS, NUMBER_OF_STEPS, BOUNCING, ALIGNMENT, SEPARATION, COHESION, WIND, WIND_SPEED, WIND_DIRECTION, GOAL, GOAL_X, GOAL_Y
     BOUNCING = True if bouncing_boolean.get() else False
     NUMBER_OF_BOIDS = int(number_of_boids_slider.get())
     label_number_of_boids.config(text=f"Boids : {NUMBER_OF_BOIDS}")
@@ -152,9 +78,123 @@ def validate_parameters():
     if WIND:
         WIND_SPEED = float(wind_speed_slider.get()/10)
         WIND_DIRECTION = int(wind_direction_slider.get())
+    GOAL = True if goal_boolean.get() else False
+    if GOAL:
+        GOAL_X = int(goal_x_slider.get())
+        GOAL_Y = int(goal_y_slider.get())
     set_recap_labels()
 
-button_validate = tkinter.Button(parameters_frame, text="Validate", font=("Helvetica", 16), command=validate_parameters)
+## Bouncing checkbox 
+bouncing_boolean = tkinter.BooleanVar()
+bouncing_checkbox = tkinter.Checkbutton(parameters_frame, text="Bouncing", font=("Helvetica", 16), variable=bouncing_boolean, onvalue=True, offvalue=False, command= lambda: (validate_parameters()))
+if BOUNCING:
+    bouncing_checkbox.select()
+bouncing_checkbox.pack()
+
+## Number of boids slider
+number_of_boids_slider = tkinter.Scale(parameters_frame, from_=1, to=1000, orient=tkinter.HORIZONTAL, length=200, label="Number of boids", font=("Helvetica", 16), command=lambda e: (validate_parameters()))
+number_of_boids_slider.set(NUMBER_OF_BOIDS)
+number_of_boids_slider.pack()
+
+## Number of steps slider
+label_number_of_steps = tkinter.Label(parameters_frame, text="Number of steps", font=("Helvetica", 16))
+label_number_of_steps.pack()
+tkinter.Label(parameters_frame, text="0 for infinite loop", font=("Helvetica", 10)).pack()
+var_number_of_steps_spinbox = tkinter.StringVar()
+var_number_of_steps_spinbox.set(NUMBER_OF_STEPS)
+number_of_steps_spinbox = tkinter.Spinbox(parameters_frame, from_=0, to=1_000_000, textvariable=var_number_of_steps_spinbox, font=("Helvetica", 16), command= lambda: (validate_parameters()))
+number_of_steps_spinbox.pack()
+
+## Forces frame
+forces_frame = tkinter.LabelFrame(parameters_frame, text="Forces", font=("Helvetica", 16))
+forces_frame.pack()
+
+### Alignment checkbox
+alignment_boolean = tkinter.BooleanVar()
+alignment_checkbox = tkinter.Checkbutton(forces_frame, text="Alignment", font=("Helvetica", 16), variable=alignment_boolean, onvalue=True, offvalue=False, command= lambda: (validate_parameters()))
+if ALIGNMENT:
+    alignment_checkbox.select() 
+alignment_checkbox.pack()
+
+### Cohesion checkbox
+cohesion_boolean = tkinter.BooleanVar()
+cohesion_checkbox = tkinter.Checkbutton(forces_frame, text="Cohesion", font=("Helvetica", 16), variable=cohesion_boolean, onvalue=True, offvalue=False, command= lambda: (validate_parameters()))
+if COHESION:
+    cohesion_checkbox.select()
+cohesion_checkbox.pack()
+
+### Separation checkbox
+separation_boolean = tkinter.BooleanVar()
+separation_checkbox = tkinter.Checkbutton(forces_frame, text="Separation", font=("Helvetica", 16), variable=separation_boolean, onvalue=True, offvalue=False, command= lambda: (validate_parameters()))
+if SEPARATION:
+    separation_checkbox.select()
+separation_checkbox.pack()
+
+### Wind checkbox
+
+def toggle_wind_parameters():
+    """ Show the wind parameters if the wind checkbox is selected """
+    if wind_boolean.get():
+        wind_parameters_frame.pack()
+    else:
+        wind_parameters_frame.pack_forget()
+
+wind_boolean = tkinter.BooleanVar()
+wind_checkbox = tkinter.Checkbutton(forces_frame, text="Wind", font=("Helvetica", 16), variable=wind_boolean, onvalue=True, offvalue=False, command=lambda: (toggle_wind_parameters(), validate_parameters()))
+
+#### Wind parameters
+wind_parameters_frame = tkinter.LabelFrame(forces_frame, text="Wind Settings", font=("Helvetica", 16))
+
+if WIND:
+    wind_checkbox.select()
+    wind_parameters_frame.pack()
+wind_checkbox.pack()
+
+##### Wind speed slider
+wind_speed_slider = tkinter.Scale(wind_parameters_frame, from_=0, to=10, orient=tkinter.HORIZONTAL, 
+    length=200, tickinterval=2, label="Wind speed", font=("Helvetica", 16), command= lambda e: (validate_parameters()))
+wind_speed_slider.set(0)
+wind_speed_slider.pack()
+
+##### Wind direction slider
+wind_direction_slider = tkinter.Scale(wind_parameters_frame, from_=0, to=360, orient=tkinter.HORIZONTAL, 
+    length=200, label="Wind direction", font=("Helvetica", 16), command= lambda e: (validate_parameters()))
+wind_direction_slider.set(0)
+wind_direction_slider.pack()
+
+### Goal checkbox
+
+def toggle_goal_parameters():
+    """ Show the goal parameters if the goal checkbox is selected """
+    if goal_boolean.get():
+        goal_parameters_frame.pack()
+    else:
+        goal_parameters_frame.pack_forget()
+
+goal_boolean = tkinter.BooleanVar()
+goal_checkbox = tkinter.Checkbutton(forces_frame, text="Goal", font=("Helvetica", 16), variable=goal_boolean, onvalue=True, offvalue=False, command= lambda: (toggle_goal_parameters(), validate_parameters()))
+
+#### Goal parameters frame
+goal_parameters_frame = tkinter.LabelFrame(forces_frame, text="Goal Settings", font=("Helvetica", 16))
+
+if GOAL:
+    goal_checkbox.select()
+    goal_parameters_frame.pack()
+goal_checkbox.pack()
+
+##### Goal x slider
+goal_x_slider = tkinter.Scale(goal_parameters_frame, from_=0, to=WIDTH, orient=tkinter.HORIZONTAL,
+    length=200, tickinterval=10, label="Goal x", font=("Helvetica", 16), command= lambda e: (validate_parameters()))
+goal_x_slider.set(0)
+goal_x_slider.pack()
+
+##### Goal y slider
+goal_y_slider = tkinter.Scale(goal_parameters_frame, from_=0, to=HEIGHT, orient=tkinter.HORIZONTAL,
+    length=200, tickinterval=10, label="Goal y", font=("Helvetica", 16), command= lambda e: (validate_parameters()))
+goal_y_slider.set(0)
+goal_y_slider.pack()
+
+button_validate = tkinter.Button(parameters_frame, text="Validate", font=("Helvetica", 16), command=lambda: validate_parameters())
 button_validate.pack()
 
 ## Left recap frame
@@ -166,6 +206,9 @@ recap_label_frame = tkinter.LabelFrame(left_recap_frame, text="Recap", font=("He
 recap_label_frame.pack()
 
 ### Display the parameters of the simulation
+label_max_iterations = tkinter.Label(recap_label_frame, text=f"Max iterations: {NUMBER_OF_STEPS}", font=("Helvetica", 16))
+label_max_iterations.pack()
+
 label_bouncing = tkinter.Label(recap_label_frame, text=f"Bouncing : {BOUNCING}\n", font=("Helvetica", 16), fg="green" if BOUNCING else "red")
 label_bouncing.pack()
 
@@ -190,10 +233,22 @@ label_wind_speed.pack()
 label_wind_direction = tkinter.Label(labelframe_wind_forces, text=f"Wind direction : {WIND_DIRECTION}", font=("Helvetica", 16))
 label_wind_direction.pack()
 
+labelframe_goal_forces = tkinter.LabelFrame(label_forces, text=f"Goal : {GOAL}\n", font=("Helvetica", 16), fg="green" if GOAL else "red")
+labelframe_goal_forces.pack()
+
+label_goal_position = tkinter.Label(labelframe_goal_forces, text=f"Goal : ({GOAL_X},{GOAL_Y})", font=("Helvetica", 16))
+label_goal_position.pack()
+
 def set_recap_labels():
     """Set the recap labels"""
-    global label_bouncing, BOUNCING, label_alignment_forces, ALIGNMENT, label_cohesion_forces, COHESION, \
-    label_separation_forces, SEPARATION, labelframe_wind_forces, WIND, label_wind_speed, WIND_SPEED, label_wind_direction, WIND_DIRECTION
+    global NUMBER_OF_STEPS, label_max_iterations, label_bouncing, BOUNCING, label_alignment_forces, ALIGNMENT, label_cohesion_forces, COHESION, \
+    label_separation_forces, SEPARATION, labelframe_wind_forces, WIND, label_wind_speed, WIND_SPEED, label_wind_direction, WIND_DIRECTION, \
+    labelframe_goal_forces, GOAL, label_goal_position, GOAL_X, GOAL_Y
+
+    if not NUMBER_OF_STEPS:
+        label_max_iterations.config(text="Max iterations :  ∞")
+    else:
+        label_max_iterations.config(text=f"Max iterations: {NUMBER_OF_STEPS}")
     label_bouncing.config(text=f"Bouncing : {BOUNCING}\n", fg="green" if BOUNCING else "red")
     label_alignment_forces.config(text=f"Alignment : {ALIGNMENT}\n", fg="green" if ALIGNMENT else "red")
     label_cohesion_forces.config(text=f"Cohesion : {COHESION}\n", fg="green" if COHESION else "red")
@@ -201,6 +256,8 @@ def set_recap_labels():
     labelframe_wind_forces.config(text=f"Wind : {WIND}\n", fg="green" if WIND else "red")
     label_wind_speed.config(text=f"Wind speed : {WIND_SPEED}")
     label_wind_direction.config(text=f"Wind direction : {WIND_DIRECTION}")
+    labelframe_goal_forces.config(text=f"Goal : {GOAL}\n", fg="green" if GOAL else "red")
+    label_goal_position.config(text=f"Goal : ({GOAL_X},{GOAL_Y})")
     
 
 def disable_parameters_on_start():
@@ -215,6 +272,9 @@ def disable_parameters_on_start():
     wind_checkbox.config(state="disable")
     wind_speed_slider.config(state="disable")
     wind_direction_slider.config(state="disable")
+    goal_checkbox.config(state="disable")
+    goal_x_slider.config(state="disable")
+    goal_y_slider.config(state="disable")
 
 def enable_parameters_on_reset():
     """Enable the parameters on reset"""
@@ -228,6 +288,9 @@ def enable_parameters_on_reset():
     wind_checkbox.config(state="normal")
     wind_speed_slider.config(state="normal")
     wind_direction_slider.config(state="normal")
+    goal_checkbox.config(state="normal")
+    goal_x_slider.config(state="normal")
+    goal_y_slider.config(state="normal")
 
 #------------------------------------------------------------------------------
 # Canvas
@@ -250,7 +313,7 @@ def start_simulation(root, number_of_boids, width, height):
     simulation_space = boid.SimulationSpace(width, height)
     # Populate the simulation space
     simulation_space.populate(number_of_boids, bouncing=BOUNCING, 
-        alignment_bool=ALIGNMENT, cohesion_bool=COHESION, separation_bool=SEPARATION, wind_bool=WIND, wind_speed=WIND_SPEED, wind_direction=WIND_DIRECTION)
+        alignment_bool=ALIGNMENT, cohesion_bool=COHESION, separation_bool=SEPARATION, wind_bool=WIND, wind_speed=WIND_SPEED, wind_direction=WIND_DIRECTION, goal_bool=GOAL, goal_x=GOAL_X, goal_y=GOAL_Y)
     # Start the simulation
     simulation_space.start_simulation(number_of_steps=NUMBER_OF_STEPS)
     create_boids_canvas(canvas, simulation_space)
@@ -266,6 +329,8 @@ def create_boids_canvas(canvas, simulation_space):
         # Create the boid on the canvas
         boid.canvas_item = canvas.create_oval(x - boid.radius, y - boid.radius, x + boid.radius, y + boid.radius, fill="green" if boid.the_chosen_one else "black")
         boid.velocity_item = canvas.create_line(x, y, x + boid.velocity[0][0], y + boid.velocity[1][0], fill="red", width=2, arrow="last")
+        if GOAL:
+            canvas.goal_item = canvas.create_rectangle(GOAL_X, GOAL_Y, GOAL_X+10, GOAL_Y+10, fill="blue")
 
 def update_canvas(canvas, simulation_space):
     """Update the canvas"""
@@ -275,6 +340,8 @@ def update_canvas(canvas, simulation_space):
         # Update the boid on the canvas
         canvas.coords(boid.canvas_item, x - boid.radius, y - boid.radius, x + boid.radius, y + boid.radius)
         canvas.coords(boid.velocity_item, x, y, x + boid.velocity[0][0], y + boid.velocity[1][0])
+        if GOAL:
+            canvas.coords(canvas.goal_item, GOAL_X, GOAL_Y, GOAL_X+10, GOAL_Y+10)
 
 def clear_canvas(simulation_space, canvas):
     """Clear the canvas"""
@@ -329,5 +396,7 @@ def reset_simulation(simulation_space, canvas):
 #------------------------------------------------------------------------------
 # Main loop
 #------------------------------------------------------------------------------
-root.state("zoomed")
+
+# the window is maximized
+root.state('zoomed')
 root.mainloop()
